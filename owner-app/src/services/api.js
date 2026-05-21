407739b0c52e434c7895a874eb178f311f978836
Dharmik Kumbhani <dharmikkumbhani1904@gmail.com>
2026-05-21 17:24:31 +0530

    change in ip

--- owner-app/src/services/api.js
+++ owner-app/src/services/api.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 import { getStoredToken } from './authService';
 
 // Change this to your backend IP when testing on a physical device
+<<<<<<< Updated upstream
 const BASE_URL = 'http://10.189.171.56:8081/api';
+=======
+const BASE_URL = 'http://192.168.1.4:8081/api';
+>>>>>>> Stashed changes
 
 const api = axios.create({
   baseURL: BASE_URL,
